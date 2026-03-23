769c0a0d38a68c00e900a987e8bb29b78e426426
TahmidShawn7 <md.tahmid022@gmail.com>
2026-03-23 18:49:02 +0600

    fix: fix the animation issue

--- 03-img-slider/script.js
+++ 03-img-slider/script.js
@@ -1,18 +1,25 @@
-let currentIndex = 0;
+const slides = document.getElementById("slides");
+
+// cloning
+const firstCloneSlide = slides.children[0].cloneNode(true);
+const lastCloneSlide =
+    slides.children[slides.children.length - 1].cloneNode(true);
+
+slides.appendChild(firstCloneSlide);
+slides.insertBefore(lastCloneSlide, slides.firstChild);
+
+// var declaration
+let currentIndex = 1;
+let isAnimating = false;
+
+slides.style.transform = `translateX(-${currentIndex * 100}%)`;
 
 function showSlide(index) {
-    const slides = document.getElementById("slides");
-    const totalSlides = slides.children.length;
-
-    if (index >= totalSlides) {
-        currentIndex = 0;
-    } else if (index < 0) {
-        currentIndex = totalSlides - 1;
-    } else {
-        currentIndex = index;
-    }
-
-    slides.style.transform = `translateX(${-currentIndex * 100}%)`;
+    if (isAnimating) return;
+    isAnimating = true;
+    slides.style.transition = "transform 0.5s ease-in-out";
+    currentIndex = index;
+    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
 
 function nextSlide() {
@@ -22,6 +29,17 @@ function prevSlide() {
     showSlide(currentIndex - 1);
 }
 
-setInterval(() => {
-    showSlide(currentIndex + 1);
-}, 3000);
+slides.addEventListener("transitionend", () => {
+    const totalLength = slides.children.length;
+    if (currentIndex === 0) {
+        slides.style.transition = "none";
+        currentIndex = totalLength - 2;
+        slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+    if (currentIndex === totalLength - 1) {
+        slides.style.transition = "none";
+        currentIndex = 1;
+        slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+    isAnimating = false;
+});
